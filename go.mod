1783a36a1e7208be7522e35bb3d9a15d366e1d25
Fakhri Rossi <rossifakhri@gmail.com>
2024-12-27 15:13:32 +0700

    Crated say_hi module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fakhri-rossi/go-mod-say-hi
+
+go 1.22.6
---
+++ say_hi.go
@@ -0,0 +1,9 @@
+package go_mod_say_hi
+
+func SayHiToMom() string {
+	return "Hi, mom!"
+}
+
+func SayHi() string {
+	return "Hi!"
+}
