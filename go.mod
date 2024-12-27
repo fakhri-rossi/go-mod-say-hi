50411aba2cc68b2ea724d55d0572f6bb59928fc7
Fakhri Rossi <rossifakhri@gmail.com>
2024-12-27 21:17:19 +0700

    update sayHi with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/fakhri-rossi/go-mod-say-hi
+module github.com/fakhri-rossi/go-mod-say-hi/v2
 
 go 1.22.6
--- say_hi.go
+++ say_hi.go
@@ -4,8 +4,13 @@ func SayHiToMom() string {
 	return "Hi, mom!"
 }
 
-func SayHi() string {
-	return "Hi!"
+// Even the change is just added one parameter, it would break the code
+// So it could be determined as major change,
+// you should bring this change with new module name,
+// as the best practice, you can add the postfix with the new version,
+// e.g: github.com/fakhri-rossi/go-mod-say-hi/v2
+func SayHi(name string) string {
+	return "Hi " + name + "!"
 }
 
 func SayHello() string {
